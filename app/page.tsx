fe7e3d31c2f9730df760b142287f2793ac10f67c
Sudipta Kumar Das <sudiptadas2000k@gmail.com>
2024-02-28 21:05:45 +0530

    main:rating component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { Room } from './Room';
-import { CollaborativeApp } from './CollaborativeApp';
 
 export default function Page() {
   return (
     <Room>
-      <CollaborativeApp />
+      <h1>Hola</h1>
     </Room>
   );
 }
